9dab600bd4ec10e7731f5f7aa18ee46be4e783e7
Kreativepardha <saradhipardha12@gmail.com>
2024-07-29 15:11:19 +0530

    finished be logic:

--- Be/src/index.ts
+++ Be/src/index.ts
@@ -3,6 +3,29 @@ import mongoose from 'mongoose'
 import jsonWebtoken from 'jsonwebtoken'
 import bcrypt from'bcrypt'
 import zod from 'zod'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import { mainRouter } from './routes'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT;
+const DB = process.env.MONGO_URL as string;
+
+app.use(express.json())
+app.use(cors())
+
+app.use("/api/v1", mainRouter)
 
 
 
+mongoose.connect(DB)
+            .then(() => {
+                console.log(`DataBase Connected at ${DB}`)
+            })
+                    .catch((err) => {
+                        console.log("DataBAse error::"+ err)
+                    })
+                    app.listen(PORT, () => {
+                        console.log(`Server connected to PORT:${PORT}`)
+                    })  
